0db7ff48431cd489641d067756f5b7edee628ccf
Jiyoon Kim <rlawldbs0911@naver.com>
2022-07-16 17:42:02 +0900

    feat: #34 signUp, login 암호화

--- myapp/service/LoginService.py
+++ myapp/service/LoginService.py
@@ -1,23 +1,38 @@
 from myapp import db
 from myapp.entity import User
+from flask_bcrypt import Bcrypt
+from flask import Flask
+import jwt
+
+app = Flask(__name__)
+bcrypt = Bcrypt(app)
 
 class LoginService:
-    def login(self, userRequest):
-        user = User(email=userRequest.email,
-                    password=userRequest.password,
-                    nickname=userRequest.nickname,
-                    check_deleted=0)
+    def login(self, loginRequest):
+        user = User.query.filter_by(email=loginRequest.email).first()
+        if not user:
+            return {
+                       "message": "User Not Found"
+                   }, 404
+        else:
+            # 암호화 후 비밀번호 비교하여 일치하면 토큰 발행
+            if bcrypt.check_password_hash(user.password, 'loginRequest.password'):
+                return {
+                           'Authorization': jwt.encode({'name': user.nickname}, "secret", algorithm="HS256")
+                           # str으로 반환하여 return
+                       }, 200
+            else:
+                return {
+                       "message": "Auth Failed"
+                   }, 500
 
-        db.session.add(user)
-        db.session.commit()
-        return user
 
-    def logout(self, userRequest):
-        user = User(email=userRequest.email,
-                    password=userRequest.password,
-                    nickname=userRequest.nickname,
-                    check__deleted=1)
-
-        db.session.add(user)
-        db.session.commit()
-        return user
+    # def logout(self, loginRequest):
+    #     user = User(email=userRequest.email,
+    #                 password=userRequest.password,
+    #                 nickname=userRequest.nickname,
+    #                 check__deleted=1)
+    #
+    #     db.session.add(user)
+    #     db.session.commit()
+    #     return user
--- myapp/service/UserService.py
+++ myapp/service/UserService.py
@@ -1,14 +1,22 @@
 from sqlalchemy.sql.operators import exists
-
+from flask_bcrypt import Bcrypt
 from myapp import db
 from myapp.entity import User
+from flask import Flask
+
+app = Flask(__name__)
+bcrypt = Bcrypt(app)
 
 class UserService:
     def save(self, userRequest):
+        # 중복 회원가입 방지
         if(self.isExist(userRequest.email)):
             raise Exception("이미 존재하는 회원입니다")
+        # 새로운 회원
+        else:
+            pw_hash = bcrypt.generate_password_hash('userRequest.password')
         user = User(email=userRequest.email,
-                    password=userRequest.password,
+                    password=pw_hash[:30],
                     nickname=userRequest.nickname)
         db.session.add(user)
         db.session.commit()
