2ceece948592ad1dff0fd5b94cfa8c69c67b6268
sm000117 <75351686+ParkSeonMi@users.noreply.github.com>
2022-07-19 16:35:37 +0900

    feat: #13 사진 단일 조회 기능 추가

--- myapp/controller/ApiRouter.py
+++ myapp/controller/ApiRouter.py
@@ -9,7 +9,7 @@ def routeApi(api):
     api.add_resource(UserController, '/api/users')
     api.add_resource(UserSingleController, '/api/users/<int:user_id>')
     api.add_resource(PhotoController, '/api/photos')
-    api.add_resource(ColorizationController, '/api/photos/<int:photo_id>')
+    api.add_resource(PhotoSingleController, '/api/photos/<int:photo_id>')
     api.add_resource(LoginController, '/api/login')
     api.add_resource(AccessController, '/api/access')
     api.add_resource(RefreshController, '/api/refresh')
