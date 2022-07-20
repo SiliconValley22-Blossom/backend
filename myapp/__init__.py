dfd54f93c8bbb681bb737ba367521c88bd1e5ccd
sm000117 <75351686+ParkSeonMi@users.noreply.github.com>
2022-07-20 17:52:09 +0900

    feat #38 refresh token, redis 연동

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -1,12 +1,11 @@
-from flask import Flask, request, jsonify
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+import redis
+from flask import Flask, jsonify
 from flask_jwt_extended import (
-JWTManager, jwt_required, create_access_token, create_refresh_token,
-get_jwt_identity, unset_jwt_cookies
+    JWTManager
 )
+from flask_migrate import Migrate
 from flask_restx import Api as DocApi
-from flask_restful import Resource, Api, fields, marshal_with
+from flask_sqlalchemy import SQLAlchemy
 
 # import Config
 
